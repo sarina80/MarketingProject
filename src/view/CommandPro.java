2652064873ce5a2487fcf8ddc347cc8f4cbd70f4
sarazm2000 <sarazm.2000@gmail.com>
2020-04-15 21:04:19 +0430

    Start the project

---
+++ src/view/CommandPro.java
@@ -0,0 +1,14 @@
+package view;
+
+public class CommandPro {
+    private String command;
+    private String commandPattern;
+
+    public CommandPro(String command) {
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+}
